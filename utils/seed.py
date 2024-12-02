7535970777f5b3962135138166c30bfa0bcdf34e
Tenzin Lhakpa <65482010+tenlhak@users.noreply.github.com>
2024-12-02 00:10:27 -0500

    Create seed.py

---
+++ utils/seed.py
@@ -0,0 +1,13 @@
+# utils/seed.py
+
+import torch
+import numpy as np
+import random
+
+def set_seed(seed=42):
+    """Set random seeds for reproducibility."""
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    random.seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
